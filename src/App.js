d58b258d583fae35d4413d50642d3af9f0a52894
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2022-01-24 00:09:26 +0200

    install

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+
+function App() {
+  return (
+		<div className='app'>
+			<div className='container'>
+
+      <div className='row'>
+          <div className='col'></div>
+          <div className='col'></div>
+          <div className='col'></div>
+          <div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+          <div className='col'></div>
+          <div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+          <div className='col'></div>
+
+				</div>
+			</div>
+			<div className='container flex-column'>
+
+				<div className='row'>
+        <div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+          <div className='col'></div>
+
+				</div>
+
+				<div className='row'>
+        <div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+
+				</div>
+			</div>
+			<div className='container'>
+
+				<div className='row'>
+          <div className='col'></div>
+          <div className='col'></div>
+          <div className='col'></div>
+          <div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+          <div className='col'></div>
+          <div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+					<div className='col'></div>
+				</div>
+			</div>
+		</div>
+  );
+}
+
+export default App;
